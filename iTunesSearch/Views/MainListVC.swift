e348f6c86ebbdc6cac841b12b20700cefa7c8792
Rhonny Gonzalez <rhonny.g@live.com>
2019-01-28 05:18:23 -0400

    feat: Added load more

--- iTunesSearch/Views/MainListVC.swift
+++ iTunesSearch/Views/MainListVC.swift
@@ -19,6 +19,8 @@ class MainListVC: UIViewController {
     private var viewModel: MainListVM!
     var mediaType: MediaType!
     var searching = false
+    var fetching = false
+    var totalOfMedia = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,20 +38,28 @@ class MainListVC: UIViewController {
         self.searchTF.delegate = self
         mediaTV.estimatedRowHeight = 150
         mediaTV.rowHeight = UITableView.automaticDimension
+        mediaTV.isHidden = true
         let button =  UIButton(type: .custom)
         button.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
         button.setTitle("Music \u{25BE}", for: .normal)
         button.addTarget(self, action: #selector(self.clickOnButton), for: .touchUpInside)
         self.navigationItem.titleView = button
         mediaType = .Music
-        fetchData()
         
     }
-    func fetchData(){
-        viewModel.fetchMediaData(term: "jack", media: mediaType.rawValue, offset: 0, limit: 2, successHandler: { (response) in
-            
+    func fetchData(offset: Int){
+        fetching = true
+        if searchTF.text! == ""{
+            fetching = false
+            self.mediaTV.isHidden = true
+            return
+        }
+        viewModel.fetchMediaData(term: searchTF.text!, media: mediaType.rawValue, offset: offset, limit: 5, successHandler: { (response) in
+            self.fetching = false
+            self.mediaTV.isHidden = false
             self.mediaTV.reloadData()
         }) { (error) in
+            self.fetching = false
         }
     }
     @objc func clickOnButton(sender: UIButton){
@@ -60,21 +70,21 @@ class MainListVC: UIViewController {
             sender.setTitle("Music \u{25BE}", for: .normal)
             self.navigationItem.titleView = sender
             self.mediaType = MediaType(rawValue: "music")
-            self.fetchData()
+            self.fetchData(offset: 0)
         }))
         
         alert.addAction(UIAlertAction(title: "TV Show", style: .default , handler:{ (UIAlertAction)in
             sender.setTitle("TV Show \u{25BE}", for: .normal)
             self.navigationItem.titleView = sender
             self.mediaType = MediaType(rawValue: "tvShow")
-            self.fetchData()
+            self.fetchData(offset: 0)
         }))
         
         alert.addAction(UIAlertAction(title: "Movie", style: .default , handler:{ (UIAlertAction)in
             sender.setTitle("Movie \u{25BE}", for: .normal)
             self.navigationItem.titleView = sender
             self.mediaType = MediaType(rawValue: "movie")
-            self.fetchData()
+            self.fetchData(offset: 0)
         }))
         
         alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
@@ -117,7 +127,8 @@ extension MainListVC: UITableViewDelegate, UITableViewDataSource{
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfMediaInSection()
+        totalOfMedia = viewModel.numberOfMediaInSection()
+        return totalOfMedia
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return viewModel.cellAtIndexPath(tableView: tableView,indexPath: indexPath)
@@ -125,9 +136,21 @@ extension MainListVC: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == totalOfMedia && !fetching{
+            fetchData(offset: totalOfMedia)
+        }
+    }
 }
 
 extension MainListVC: UITextFieldDelegate{
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text != ""{
+            self.view.endEditing(true)
+            fetchData(offset: 0)
+        }
+        return true
+    }
     
 }
